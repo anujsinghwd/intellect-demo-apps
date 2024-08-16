8ec7d2e48e23c1b03449a0188a381c98a32983c5
Anujsi (Ext.Coforge) <anuj_singh@outlook.in>
2024-08-16 20:04:24 +0530

    update feedback

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 import "./feedback.css";
 
-
 const emojiData = [
-  {name: 'Terrible',emoji: '😢'},
-  {name: 'Bad',emoji: '😞'},
-  {name: 'Alright',emoji: '😐'},
-  {name: 'Pretty Good',emoji: '😊'},
-  {name: 'Fantastic',emoji: '😁'}
-]
+  { name: "Terrible", emoji: "😢" },
+  { name: "Bad", emoji: "😞" },
+  { name: "Alright", emoji: "😐" },
+  { name: "Pretty Good", emoji: "😊" },
+  { name: "Fantastic", emoji: "😁" },
+];
 
 const WellbeingWidget = () => {
   const [selectedEmoji, setSelectedEmoji] = useState(null);
   const handleEmojiClick = (index) => {
     setSelectedEmoji(index);
+  };
+
+  const handleBtnClick = () => {
+    alert("Continue.......");
   };
   return (
     <>
@@ -21,9 +24,8 @@ const WellbeingWidget = () => {
         <h2>Select Your Mood</h2>
         <div className="emoji-scale">
           {emojiData.map((emoji, index) => (
-            <div className="emoji-container">
+            <div className="emoji-container" key={emoji.name}>
               <div
-                key={emoji.name}
                 className={`emoji-item ${
                   selectedEmoji === index ? "selected" : ""
                 }`}
@@ -37,9 +39,14 @@ const WellbeingWidget = () => {
         </div>
         {selectedEmoji !== null && (
           <div className="selected-emoji-text">
-            You selected: <span>{emojiData[selectedEmoji]['emoji']}</span>
+            You selected: <span>{emojiData[selectedEmoji]["emoji"]}</span>
           </div>
         )}
+      </div>
+      <div className="btn-container">
+        <button className="continue-btn" onClick={handleBtnClick}>
+          Continue
+        </button>
       </div>
     </>
   );
